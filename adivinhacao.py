c7b192cffb5eb5bbf5522fed6970295fa79529a7
gdarlan <gabriel.darlan@hotmail.com>
2019-04-07 23:55:20 -0300

    Encerrando a intereacao e o loop

--- adivinhacao.py
+++ adivinhacao.py
@@ -6,11 +6,14 @@ numero_secreto = 42
 total_de_tentativas = 3
 rodada = 1
 
-""" while(rodada <= total_de_tentativas): """
 for rodada in range(1, total_de_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
-    chute = int(input("Digite o seu número: "))
-    print("Você digitou {}".format(numero_secreto))
+    chute = int(input("Digite um número entre 1 e 100: "))
+    print("Você digitou {}".format(chute))
+
+    if(chute < 1 or chute > 100):
+        print("Você deve digitar um número entre 1 e 100")
+        continue
 
     acertou = chute == numero_secreto
     maior = chute > numero_secreto
@@ -18,6 +21,7 @@ for rodada in range(1, total_de_tentativas + 1):
 
     if(acertou):
         print("Você acertou!")
+        break
     else:
         if(maior):
             print("Você errou! O seu chute foi maior que o número secreto")
