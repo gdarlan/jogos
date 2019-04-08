7ff5c72d20d1b8456dbed3fa75fe6128f0271c65
gdarlan <gabriel.darlan@hotmail.com>
2019-04-07 23:06:32 -0300

    repetição do jogo

--- adivinhacao.py
+++ adivinhacao.py
@@ -3,20 +3,26 @@ print("Bem vindo no jogo de Adivinhação")
 print("********************************")
 
 numero_secreto = 42
+total_de_tentativas = 3
+rodada = 1
 
-chute = int(input("Digite o seu número: "))
-print("Você digitou {}".format(numero_secreto))
+while(rodada <= total_de_tentativas):
+    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+    chute = int(input("Digite o seu número: "))
+    print("Você digitou {}".format(numero_secreto))
 
-acertou = chute == numero_secreto
-maior = chute > numero_secreto
-menor = chute < numero_secreto
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
 
-if(acertou):
-    print("Você acertou!")
-else:
-    if(maior):
-        print("Você errou! O seu chute foi maior que o número secreto")
-    elif(menor):
-        print("Você errou! O seu chute foi menor que o número secreto")
+    if(acertou):
+        print("Você acertou!")
+    else:
+        if(maior):
+            print("Você errou! O seu chute foi maior que o número secreto")
+        elif(menor):
+            print("Você errou! O seu chute foi menor que o número secreto")
+
+    rodada = rodada + 1
 
 print("Fim do jogo")
