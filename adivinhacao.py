ae008972dec80d6ebabcb8553280c2f5a2ca7db5
gdarlan <gabriel.darlan@hotmail.com>
2019-04-08 21:14:56 -0300

    Fim do jogo adivinhacao

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,10 +1,23 @@
+import random
+
 print("********************************")
 print("Bem vindo no jogo de Adivinhação")
 print("********************************")
 
-numero_secreto = 42
-total_de_tentativas = 3
-rodada = 1
+numero_secreto = random.randrange(1, 101)
+total_de_tentativas = 0
+pontos = 1000
+
+print("Qual nível de dificuldade?")
+print("(1) Fácil (2) Médio (3) Difícil")
+nivel = int(input("Defina um nível: "))
+
+if(nivel == 1):
+    total_de_tentativas = 20
+elif(nivel == 2):
+    total_de_tentativas = 10
+else:
+    total_de_tentativas = 5
 
 for rodada in range(1, total_de_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
@@ -20,12 +33,14 @@ for rodada in range(1, total_de_tentativas + 1):
     menor = chute < numero_secreto
 
     if(acertou):
-        print("Você acertou!")
+        print("Você acertou e {} pontos!".format(pontos))
         break
     else:
         if(maior):
             print("Você errou! O seu chute foi maior que o número secreto")
         elif(menor):
             print("Você errou! O seu chute foi menor que o número secreto")
-
+        pontos_perdidos = abs(numero_secreto - chute) 
+        pontos = pontos - pontos_perdidos
+    
 print("Fim do jogo")
---
+++ jogos.py
@@ -0,0 +1,17 @@
+import jogos.forca
+import jogos.adivinhacao
+
+print("********************************")
+print("*******Escolha o seu jogo*******")
+print("********************************")
+
+print("(1) Forca (2) Adivinhação")
+jogo = int(input("Qual o jogo?"))
+
+if(jogo == 1):
+    print("Jogando Forca")
+elif(jogo == 2):
+    print("Jogando Forca")
+
+
+print("Fim do jogo")
