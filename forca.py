98c8354f8933a832050ec39aa8ced830869985bb
gdarlan <gabriel.darlan@hotmail.com>
2019-04-11 22:07:54 -0300

    Capitulo 6 completo

--- forca.py
+++ forca.py
@@ -1,24 +1,51 @@
+import random
+
+
 def jogar():
     print("********************************")
     print("***Bem vindo no jogo de Forca***")
     print("********************************")
 
-    palavra_secreta = "banana"
+    arquivo = open("palavras.txt", "r")
+    palavras = []
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+    arquivo.close()
+
+    numero = random.randrange(0, len(palavras))
+    palavra_secreta = palavras[numero].upper()
+
+    letras_acertadas = ["_" for letra in palavra_secreta]
 
     enforcou = False
     acertou = False
+    erros = 0
+
+    print(letras_acertadas)
 
     while(not enforcou and not acertou):
 
         chute = input("Qual a letra? ")
-        chute = chute.strip()
-        index = 0
-        for letra in palavra_secreta:
-            if(chute.upper() == letra.upper()):
-                print("Encontrei a letra {} na posição {}".format(letra, index))
-            index = index + 1
-        print("jogando...")
+        chute = chute.strip().upper()
 
+        index = 0
+        if(chute in palavra_secreta):
+            for letra in palavra_secreta:
+                if(chute.upper() == letra.upper()):
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
+
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
+        print(letras_acertadas)
+
+    if(acertou):
+        print("Você ganhou!!")
+    else:
+        print("Você perdeu!!")
     print("Fim do jogo")
 
 
